2971dbfa7ce462e7d8dc01381dcd17312686543f
Danielle <ukka.ukka.nuka.nuka.appu.wa@gmail.com>
2020-05-02 15:10:01 -0700

    Got webpages loading properly.

--- Develop/server.js
+++ Develop/server.js
@@ -1,38 +1,31 @@
 // console.log("From server.js");
 
-var http = require("http");
-var PORT = 8080;
-var server = http.createServer(handle);
-var fs = require("fs");
-
-function handle(request, response){
-    var path = request.url;
- 
-    displayHome(response);
+// var http = require("http");
+ var PORT = 8080;
+// //var server = http.createServer(handle);
+// var fs = require("fs");
+var express = require("express");
+var path = require("path");
 
 
-}
+var app = express();
+
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 
 
-
-function displayHome(response){
-   read("index.html", response);
-}
-
-
-function read(filename, response){
-    fs.readFile(__dirname + "/public/" + filename, function(err, data){
-        if (err) throw err;
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.end(data);
-    });
-}
-
-server.listen(PORT, function(){
-   // console.log("Server listening on: http://localhost:" + PORT);
+app.get("/", function(req, response){
+    response.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 
+app.get("/notes", function (req, response){
+    response.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
 
+app.listen(PORT, function(){
+    console.log("Listening on PORT: http://" + PORT);
+});
