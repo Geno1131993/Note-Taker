456e28ef47e57231edde361cd4d6103f4ecae7be
Danielle <ukka.ukka.nuka.nuka.appu.wa@gmail.com>
2020-05-02 18:37:23 -0700

    Issues with console.log/reading file from db.

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,8 @@
-// console.log("From server.js");
 
 // var http = require("http");
- var PORT = 8080;
+ var PORT = 3000;
 // //var server = http.createServer(handle);
-// var fs = require("fs");
+var fs = require("fs");
 var express = require("express");
 var path = require("path");
 
@@ -17,15 +16,40 @@ app.use(express.json());
 
 
 app.get("/", function(req, response){
-    response.sendFile(path.join(__dirname, "/public/index.html"));
+
+    return response.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 
 app.get("/notes", function (req, response){
-    response.sendFile(path.join(__dirname, "/public/notes.html"));
+
+    return response.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+
+app.get("*", function (req, response){
+    return response.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 app.listen(PORT, function(){
-    console.log("Listening on PORT: http://" + PORT);
+    console.log("Listening on PORT: localhost:" + PORT);
 });
+
+
+
+//Where is it printing out?
+
+app.get("/api/notes", function(req, response){
+    console.log("From get api/notes");
+    console.log(read("/db/db.json"));
+    return response.sendFile(path.join(__dirname, "public.notes.html"));
+});
+
+
+
+function read(filename){
+    fs.readFile(filename, function(err, text){
+        console.log(`From read: \n${text}`);
+        return text;
+    });
+}
